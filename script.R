713ebe52ecdb52e68397ac838ac5e705f9e7278a
Cem <cemo.ustundag@gmail.com>
2022-05-20 17:05:06 +0100

    p1t4 done

--- script.R
+++ script.R
@@ -1,6 +1,7 @@
 ### PROJECT 1
 
 ## TASK 1
+
 data <- read.csv("office.csv", stringsAsFactors = FALSE)
 
 ## TASK 2
@@ -27,3 +28,16 @@ hier_clust <- hclust(dist, method = "ward.D2")
 
 # plot dendogram
 plot(hier_clust)
+
+## TASK 4
+
+# 6-cluster visualisation
+rect.hclust(hier_clust, k = 6, border = "red")
+
+# 6-cluster solution
+hcluster_groups <- cutree(hier_clust, k = 6)
+
+# number of obs per cluster
+table(hcluster_groups)
+
+## TASK 5
