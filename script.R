de11d912611079643fccf431d95e462ac46cea60
Cem <cemo.ustundag@gmail.com>
2022-05-20 17:22:02 +0100

    p1t8 done

--- script.R
+++ script.R
@@ -1,3 +1,8 @@
+### LIBRARIES
+
+library(tidyverse)
+library(flexclust)
+
 ### PROJECT 1
 
 ## TASK 1
@@ -41,3 +46,36 @@ hcluster_groups <- cutree(hier_clust, k = 6)
 table(hcluster_groups)
 
 ## TASK 5
+
+# add cluster assignment column to normalised dataset
+data_att_norm <- data_att_norm %>% 
+  mutate(hcluster_groups = hcluster_groups)
+
+# means for individual clusters 
+data_att_norm %>%
+  group_by(hcluster_groups) %>% # group by cluster
+  summarise_all(~ mean(.x)) %>% # calculate the mean per group 
+  print(width = Inf) # prints all variables (all columns)
+
+# flexclust profiles 
+hier_clust_flex <- as.kcca(hier_clust, data_att_norm, k = 6)
+
+table(hcluster_groups, clusters(hier_clust_flex))
+
+barchart(hier_clust_flex, main = "Segment Profiles")
+
+## TASK 6
+
+## TASK 7
+
+## TASK 8
+
+# 5-cluster solution
+hcluster_groups_5 <- cutree(hier_clust, k = 5)
+
+# number of obs per cluster
+table(hcluster_groups_5)
+
+## TASK 9
+
+
