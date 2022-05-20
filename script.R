bf08853721055232d344dbdeaf02df9663b9aec7
Cem <cemo.ustundag@gmail.com>
2022-05-20 16:35:14 +0100

    project 1 init, task 2 done

---
+++ script.R
@@ -0,0 +1,18 @@
+data <- read.csv("office.csv", stringsAsFactors = FALSE)
+
+## task 2
+
+# attitudinal variable dataset
+data_att <- data[, 2:7]
+
+# z-score standardisation
+for(i in 1:ncol(data_att)){
+  for(n in 1:nrow(data_att)){
+    m <- mean(data_att[, i])
+    s <- sd(data_att[, i])
+    data_att[n, i] <- (data_att[n, i] - m) - s
+  }
+}
+
+# inspect for smallest min (low_prices) and largest max (return_policy)
+summary(data_att)
