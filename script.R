c8124ebad4a7be22d1571f3ee11ab19f52bfeeb8
Cem <cemo.ustundag@gmail.com>
2022-05-20 17:39:53 +0100

    clean up, datasets separated, comments added

--- script.R
+++ script.R
@@ -48,17 +48,17 @@ table(hcluster_groups)
 ## TASK 5
 
 # add cluster assignment column to normalised dataset
-data_att_norm <- data_att_norm %>% 
+data_att_norm_p1t5 <- data_att_norm %>% 
   mutate(hcluster_groups = hcluster_groups)
 
 # means for individual clusters 
-data_att_norm %>%
+data_att_norm_p1t5 %>%
   group_by(hcluster_groups) %>% # group by cluster
   summarise_all(~ mean(.x)) %>% # calculate the mean per group 
   print(width = Inf) # prints all variables (all columns)
 
 # flexclust profiles 
-hier_clust_flex <- as.kcca(hier_clust, data_att_norm, k = 6)
+hier_clust_flex <- as.kcca(hier_clust, data_att_norm_p1t5, k = 6)
 
 table(hcluster_groups, clusters(hier_clust_flex))
 
@@ -71,7 +71,7 @@ barchart(hier_clust_flex, main = "Segment Profiles")
 ## TASK 8
 
 # remove previous clustering
-data_att_norm <- data_att_norm[, -7]
+data_att_norm_p1t8 <- data_att_norm
 
 # 5-cluster solution
 hcluster_groups_5 <- cutree(hier_clust, k = 5)
@@ -82,17 +82,17 @@ table(hcluster_groups_5)
 ## TASK 9
 
 # add cluster assignment column to normalised dataset
-data_att_norm <- data_att_norm %>% 
+data_att_norm_p1t9 <- data_att_norm %>% 
   mutate(hcluster_groups_5 = hcluster_groups_5)
 
 # means for individual clusters 
-data_att_norm %>%
+data_att_norm_p1t9 %>%
   group_by(hcluster_groups_5) %>% # group by cluster
   summarise_all(~ mean(.x)) %>% # calculate the mean per group 
   print(width = Inf) # prints all variables (all columns)
 
 # flexclust profiles 
-hier_clust_flex_5 <- as.kcca(hier_clust, data_att_norm, k = 5)
+hier_clust_flex_5 <- as.kcca(hier_clust, data_att_norm_p1t9, k = 5)
 
 table(hcluster_groups_5, clusters(hier_clust_flex_5))
 
@@ -105,13 +105,13 @@ barchart(hier_clust_flex_5, main = "Segment Profiles")
 ## TASK 12
 
 # remove hierarchical clustering assignment col
-data_att_norm <- data_att_norm[, -7]
+data_att_norm_p1t12 <- data_att_norm
 
 # set seed
 set.seed(123)
 
 # k means clustering
-kmeans_clust <- kmeans(data_att_norm, 
+kmeans_clust <- kmeans(data_att_norm_p1t12, 
                        centers = 5, 
                        iter.max = 1000,
                        nstart = 100)
@@ -120,4 +120,13 @@ table(kmeans_clust$cluster)
 
 ## TASK 13
 
+# add k means clustering groups to normalised dataset
+data_att_norm_p1t13 <- data_att_norm %>% 
+  mutate(kcluster_groups = kmeans_clust$cluster)
+
+# means for each cluster
+data_att_norm_p1t13 %>%
+  group_by(kcluster_groups) %>% # group by cluster
+  summarise_all(~ mean(.x)) %>% # calculate the mean per group 
+  print(width = Inf) # prints all variables (all columns)
 
