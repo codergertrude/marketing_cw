93195d7903b067efc77916051d1818c02d5f46b4
Cem <cemo.ustundag@gmail.com>
2022-05-21 18:21:02 +0100

    p3t12 done

--- project3.R
+++ project3.R
@@ -99,3 +99,37 @@ predicted_probabilities <- predict(model2, m_data) %>%
 predicted_probabilities
 
 ## TASK 10
+
+predicted_alt <- data.frame(prediction = numeric())
+
+# mapping most likely choice as prediction (prediction for choice set 3 is 2)
+for(i in 1:nrow(predicted_probabilities)){
+  if(predicted_probabilities[i, 1] > predicted_probabilities[i, 2] & predicted_probabilities[i, 1] > predicted_probabilities[i, 3]){
+    predicted_alt[i, 1] <- 1
+  } else if(predicted_probabilities[i, 2] > predicted_probabilities[i, 1] & predicted_probabilities[i, 2] > predicted_probabilities[i, 3]){
+    predicted_alt[i, 1] <- 2
+  } else {
+    predicted_alt[i, 1] <- 3
+  }
+}
+
+## TASK 11
+
+# extracting selections from original dataset (selected alternative for 15th choice set is 2)
+selected_alt <- as.data.frame(data_p3t4$alternative_id)
+
+## TASK 12
+
+# accuracy calculation (60.56667% calculated accuracy)
+good_pred_hyp <- 0
+
+for(i in 1:nrow(selected_alt)){
+  if(selected_alt[i, 1] == predicted_alt[i, 1]){
+    good_pred_hyp <- 1 + good_pred_hyp
+  }
+}
+
+accuracy_hyp <- good_pred_hyp/nrow(selected_alt)
+accuracy_hyp*100
+
+## TASK 13
